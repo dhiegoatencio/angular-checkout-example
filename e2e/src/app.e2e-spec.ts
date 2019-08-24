ec9c56f86addb2d8d1427499235d11564c81ba74
Dhiego Hendrix Atencio <dhiego.atencio@gmail.com>
2019-08-23 22:07:42 -0300

    removed automatically generated examples

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,6 @@
-import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
+
+import { AppPage } from './app.po';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -8,10 +9,7 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-checkout-example app is running!');
-  });
+  // it's
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser

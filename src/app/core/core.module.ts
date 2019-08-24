8dab9d94c25aefa4112b82220296ac3698451e0c
Dhiego Hendrix Atencio <dhiego.atencio@gmail.com>
2019-08-24 13:33:22 -0300

    added footer and some menu information

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,3 +1,4 @@
+import { AppFooterComponent } from './app-footer/app-footer.component';
 import { AppMenuComponent } from './app-menu/app-menu.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
@@ -5,12 +6,14 @@ import { NgModule } from '@angular/core';
 @NgModule({
   declarations: [
     AppMenuComponent,
+    AppFooterComponent,
   ],
   imports: [
     CommonModule,
   ],
   exports: [
     AppMenuComponent,
+    AppFooterComponent,
   ]
 })
 export class CoreModule { }

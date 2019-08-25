883f2561d2306311035bd42c36ec3477f09fbd2e
Dhiego Hendrix Atencio <dhiego.atencio@gmail.com>
2019-08-25 14:17:27 -0300

    form validations have been added

--- src/app/shared/components/bxs-checkout/bxs-checkout-form/bxs-checkout-form.component.ts
+++ src/app/shared/components/bxs-checkout/bxs-checkout-form/bxs-checkout-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'bxs-checkout-form',
@@ -7,7 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BxsCheckoutFormComponent implements OnInit {
 
-  card = {};
+  cardForm = new FormGroup({
+    number: new FormControl('', [
+      Validators.required,
+    ]),
+    name: new FormControl('', Validators.required),
+    date: new FormControl('', [
+      Validators.required,
+      Validators.pattern('(1[0-2]|0[1-9])\/(1[5-9]|2\d)'),
+    ]),
+    cvv: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[0-9]{3}'),
+    ]),
+    installments: new FormControl('', Validators.required),
+  });
 
   constructor() { }
 
--- src/app/shared/components/bxs-checkout/bxs-checkout.module.ts
+++ src/app/shared/components/bxs-checkout/bxs-checkout.module.ts
@@ -1,7 +1,8 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { BxsCheckoutComponent } from './bxs-checkout.component';
 import { BxsCheckoutFormComponent } from './bxs-checkout-form/bxs-checkout-form.component';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -20,6 +21,7 @@ import { NgModule } from '@angular/core';
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
+    ReactiveFormsModule,
   ],
   exports: [
     BxsCheckoutComponent

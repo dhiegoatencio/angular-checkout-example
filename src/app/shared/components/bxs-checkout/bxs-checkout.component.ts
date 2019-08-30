500abf4ca59a1900287fec68e259ba58f0cd8151
Dhiego Hendrix Atencio <dhiego.atencio@gmail.com>
2019-08-29 21:11:39 -0300

    css BEM and card layout

--- src/app/shared/components/bxs-checkout/bxs-checkout.component.ts
+++ src/app/shared/components/bxs-checkout/bxs-checkout.component.ts
@@ -16,11 +16,27 @@ export class BxsCheckoutComponent {
   paymentType = 'card';
 
   cardNumber: string;
+  cardName: string;
+  cardDate: string;
 
   constructor() { }
 
-  handleFormChanges(values: BxsCheckoutForm) {
-    this.cardNumber = values.number;
+  handleFormChanges({number, date, name, cvv}: BxsCheckoutForm) {
+
+    this.cardName = name;
+  
+    // build card number with mask
+    const group2 = number.substr(4, 4);
+    const group3 = number.substr(8, 4);
+    const group4 = number.substr(12, 4);
+    this.cardNumber = number.substr(0, 4)
+      + (group2 && '&nbsp;&nbsp' + group2)
+      + (group3 && '&nbsp;&nbsp' + group3)
+      + (group4 && '&nbsp;&nbsp' + group4);
+
+    // build card date with mask
+    const year = date.substr(2, 2);
+    this.cardDate = date.substr(0, 2) + (year && '/' + year);
   }
 
   handleBackToPaymentSelection() {
